ad912c96b1c3fd7396e0de43bc18ae59effee05e
Huy Ta <huytbt@gmail.com>
2020-09-18 01:06:01 +0700

    Show lunar date on calendar

--- lib/calendar.dart
+++ lib/calendar.dart
@@ -66,38 +66,13 @@ class _CalendarScreenState extends State<CalendarScreen> {
             TableRow(
               children: [
                 TableCell(
-                  child: showDate(28),
+                  child: showDate(28, otherMonth: true),
                 ),
                 TableCell(
-                  child: showDate(29),
+                  child: showDate(29, otherMonth: true),
                 ),
                 TableCell(
-                  child: showDate(30),
-                ),
-                TableCell(
-                  child: showDate(_date++),
-                ),
-                TableCell(
-                  child: showDate(_date++),
-                ),
-                TableCell(
-                  child: showDate(_date++),
-                ),
-                TableCell(
-                  child: showDate(_date++),
-                ),
-              ],
-            ),
-            TableRow(
-              children: [
-                TableCell(
-                  child: showDate(_date++),
-                ),
-                TableCell(
-                  child: showDate(_date++),
-                ),
-                TableCell(
-                  child: showDate(_date++),
+                  child: showDate(30, otherMonth: true),
                 ),
                 TableCell(
                   child: showDate(_date++),
@@ -148,6 +123,31 @@ class _CalendarScreenState extends State<CalendarScreen> {
                 ),
                 TableCell(
                   child: showDate(_date++),
+                ),
+                TableCell(
+                  child: showDate(_date++),
+                ),
+                TableCell(
+                  child: showDate(_date++),
+                ),
+                TableCell(
+                  child: showDate(_date++),
+                ),
+                TableCell(
+                  child: showDate(_date++),
+                ),
+              ],
+            ),
+            TableRow(
+              children: [
+                TableCell(
+                  child: showDate(_date++),
+                ),
+                TableCell(
+                  child: showDate(_date++),
+                ),
+                TableCell(
+                  child: showDate(_date++, today: true, selected: true),
                 ),
                 TableCell(
                   child: showDate(_date++),
@@ -166,7 +166,7 @@ class _CalendarScreenState extends State<CalendarScreen> {
             TableRow(
               children: [
                 TableCell(
-                  child: showDate(_date++),
+                  child: showDate(_date++, selected: true),
                 ),
                 TableCell(
                   child: showDate(_date++),
@@ -181,10 +181,10 @@ class _CalendarScreenState extends State<CalendarScreen> {
                   child: showDate(_date++),
                 ),
                 TableCell(
-                  child: showDate(1),
+                  child: showDate(1, otherMonth: true),
                 ),
                 TableCell(
-                  child: showDate(2),
+                  child: showDate(2, otherMonth: true),
                 ),
               ],
             ),
@@ -206,28 +206,66 @@ class _CalendarScreenState extends State<CalendarScreen> {
     );
   }
 
-  Widget showDate(int date, {bool today = false}) {
-    if (today) {
-      return Container(
-        padding: EdgeInsets.all(3),
-        child: CircleAvatar(
-          backgroundColor: Colors.red,
-          child: Center(
-            child: Text(
-              date.toString(),
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 14,
-              ),
-            ),
+  Widget showDate(
+    int date, {
+    bool today = false,
+    bool otherMonth = false,
+    bool selected = false,
+  }) {
+    bool _darkMode = Theme.of(context).brightness == Brightness.dark;
+    Color _backgroundColor = _darkMode ? Colors.white : Colors.black;
+    Color _textColor = _darkMode ? Colors.black : Colors.white;
+    return Container(
+      padding: EdgeInsets.all(3),
+      child: CircleAvatar(
+        backgroundColor: selected
+            ? (today ? Colors.red : _backgroundColor)
+            : Colors.transparent,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              otherMonth
+                  ? Text(
+                      ((date - 10) % 30 + 1).toString(),
+                      style: TextStyle(
+                        color: Colors.grey[!_darkMode ? 300 : 800],
+                        fontSize: 14,
+                      ),
+                    )
+                  : Text(
+                      ((date - 10) % 30 + 1).toString(),
+                      style: TextStyle(
+                        color: selected
+                            ? (today ? Colors.white : _textColor)
+                            : (today
+                                ? Colors.red
+                                : Theme.of(context).textTheme.bodyText1.color),
+                        fontSize: 14,
+                      ),
+                    ),
+              otherMonth
+                  ? Text(
+                      date.toString(),
+                      style: TextStyle(
+                        color: Colors.grey[!_darkMode ? 300 : 800],
+                        fontSize: 10,
+                      ),
+                    )
+                  : Text(
+                      date.toString(),
+                      style: TextStyle(
+                        color: selected
+                            ? (today ? Colors.white : _textColor)
+                            : (today
+                                ? Colors.red
+                                : Theme.of(context).textTheme.bodyText1.color),
+                        fontSize: 10,
+                      ),
+                    ),
+            ],
           ),
         ),
-      );
-    }
-    return Container(
-      padding: EdgeInsets.all(15),
-      child: Center(
-        child: Text(date.toString()),
       ),
     );
   }
